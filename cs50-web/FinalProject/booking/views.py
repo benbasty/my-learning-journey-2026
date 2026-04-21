1fe2705e17e4ed131834ae535f28efd3969a212e
benbasty <benbasty@yahoo.fr>
2026-04-21 20:28:50 +0800

    Tested initial views and templates by creating urls.py, views.py and index.html

--- cs50-web/FinalProject/booking/views.py
+++ cs50-web/FinalProject/booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import User, Category, ClassType, ScheduledClass, Booking
 
 # Create your views here.
+
+def index(request):
+    upcoming_classes = ScheduledClass.objects.filter(start_time = timezone.now()).order_by('start_time')
+    return render(request, 'booking/index.html', {
+        'classes' : upcoming_classes
+    })
