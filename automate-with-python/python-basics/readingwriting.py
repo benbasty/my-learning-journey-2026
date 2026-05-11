644166853585093d9673a1776de338fc368425a5
benbasty <benbasty@yahoo.fr>
2026-05-11 22:49:03 +0800

    Add state capitals quizzes and random quiz generator script

--- automate-with-python/python-basics/readingwriting.py
+++ automate-with-python/python-basics/readingwriting.py
@@ -2,9 +2,14 @@
 # The Path() function in the pathlib module handles all operating systems
 from pathlib import Path
 # accessing the home directory
-print(Path.home())
+# print(Path.home())
 # creating new folders
-import os
+# import os
 # os.makedirs('automating-tasks')
 
 # The File Reading and Writing Process
+#write text and read text
+
+p = Path('hello.txt')
+p.write_text('Hello, Nice to meet u')
+print(p.read_text())
