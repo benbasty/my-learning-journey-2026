9bb69cd7136d6392674e0bcd5fd2defd1617e8c9
benbasty <benbasty@yahoo.fr>
2026-05-11 11:05:08 +0800

    Enhance Pig Latin translator to handle non-letter characters and add reading/writing example script

--- automate-with-python/python-basics/piglatin.py
+++ automate-with-python/python-basics/piglatin.py
@@ -10,13 +10,26 @@ print('Pls Enter the English message to translate into pig latin:')
 message = input('> ')
 VOWELS = ('a', 'e', 'i', 'o', 'u', 'y')
 pig_latin = [] # A list of the words in pig latin
+
+# get a list of the words as separate strings.
+# message.split returns ['My', 'name', 'is', 'AL', 'SWEIGART', 'and', 'I', 'am', '4,000', 'years', 'old.'].
 for word in message.split():
     # Separate the non-letters at the start of this word:
+    # remove any non-letters from the start and end of each word
+    # so that strings like 'old.' translate to 'oldyay.' instead of 'old.yay'
+    # We save these non-letters to a variable named prefix_non_letters.
     prefix_non_letters = ''
+
+    # This loop continues as long as the string isn't empty and the first character is not a letter
+    # .isalpha() returns True if the character is a letter, and False otherwise.
     while len(word) > 0 and not word[0].isalpha():
+        # It takes that non-letter character and saves it into prefix_non_letters
         prefix_non_letters += word[0]
+        # It "slices" the string, removing that first character so the loop can check the next one.
         word = word[1:]
+    # After the loop finishes, this checks if there is anything left of the word.
     if len(word) == 0:
+        # If the word was only non-letters, it adds those symbols to the results list
         pig_latin.append(prefix_non_letters)
         continue
 
---
+++ automate-with-python/python-basics/readingwriting.py
@@ -0,0 +1,10 @@
+#import Path
+# The Path() function in the pathlib module handles all operating systems
+from pathlib import Path
+# accessing the home directory
+print(Path.home())
+# creating new folders
+import os
+# os.makedirs('automating-tasks')
+
+# The File Reading and Writing Process
