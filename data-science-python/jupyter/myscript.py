f7548ee001b2f00b69ff93cb296b235f048d16b2
benbasty <benbasty@yahoo.fr>
2026-05-30 11:42:02 +0800

    Add comments explaining %xmode magic function in myscript.py

--- data-science-python/jupyter/myscript.py
+++ data-science-python/jupyter/myscript.py
@@ -2,3 +2,11 @@ def square(x):
     return x ** 2
 for num in range(1,4):
     print(f"{num} squared is {square(num)}")
+
+# %xmode magic function (short for exception mode)
+# %xmode takes a single argument, the mode,
+# and there are three possibilities: Plain, Context, and Verbose.
+# The default is Context, which gives output like that just shown.
+# Plain is more compact and gives less information
+# Verbose mode adds some extra information,
+# including the arguments to any functions that are called
