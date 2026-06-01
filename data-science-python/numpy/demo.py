d4f55b49314ea743c18f24af8cb985e995a6b1e9
benbasty <benbasty@yahoo.fr>
2026-06-01 22:43:20 +0800

    Add initial dataset handling explanations in demo.py

---
+++ data-science-python/numpy/demo.py
@@ -0,0 +1,11 @@
+# datasets can come from a wide range of sources and in a wide range of formats
+# many datasets can be represented fundamentally as arrays of numbers.
+# No matter what the data is, to make it analyzable we have to transform it into arrays of numbers.
+# efficient storage and manipulation of numerical arrays is absolutely fundamental
+# to the process of doing data science
+# to handle such numerical arrays: we use the NumPy package and the Pandas package
+
+
+# Numpy (Numerical Python) provides a way to store and operate on dense data buffers
+# NumPy arrays are like Python’s built-in list type
+# NumPy arrays provide much more efficient storage and data operations as the arrays grow larger in size.
