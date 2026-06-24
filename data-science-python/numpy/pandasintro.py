985bd80916d889d313ff5b865d36df76ed5399fc
benbasty <benbasty@yahoo.fr>
2026-06-24 09:01:52 +0800

    Add examples for constructing DataFrame objects and using Pandas Index

--- data-science-python/numpy/pandasintro.py
+++ data-science-python/numpy/pandasintro.py
@@ -68,8 +68,46 @@ statesid = states.index
 states['area']
 
 # Constructing DataFrame Objects from a single Series object
-ap = pd.DataFrame(population, columns='population')
+ap = pd.DataFrame(population, columns=['population'])
 
 # Constructing DataFrame Objects from a list of dicts
 data4 = [{'a': i, 'b': 2 * i} for i in range(3)]
 dd4 = pd.DataFrame(data)
+
+# Constructing DataFrame Objects from a dictionnary of Series Objects
+st = pd.DataFrame({'population':population, 'area': area})
+
+# Constructing DataFrame Objects From a two-dimensional Numpy array
+ci = pd.DataFrame(np.random.rand(3,2),
+                  columns=['foo','bar'],
+                  index=['a','b','c'])
+
+# Constructing DataFrame Objects From a NumPy structured array
+A = np.zeros(3, dtype=[('A','i8'), ('B', 'f8')])
+AD = pd.DataFrame(A)
+
+# PANDAS INDEX OBJECT
+
+# The Index object is either as an immutable array or as an ordered set
+# (technically a multiset, as Index objects may contain repeated values)
+
+ind = pd.Index([2, 3, 5, 7, 11])
+i1 = ind[1]
+i02 = ind[::2]
+ssnd = print(ind.size, ind.shape, ind.ndim, ind.dtype)
+
+#index does not support mutable operations
+
+# Index object as ordered set
+indA = pd.Index([1, 3, 5, 7, 9])
+indB = pd.Index([2, 3, 5, 7, 11])
+
+# Pandas can help facilitate your operations such as
+# joins accross datasets
+# the many conventions that the Index Object follows such as
+# unions, intersections, differences and others can be computed in the same way
+
+inter = indA.intersection(indB) #returns a new set of index common to the index objects
+unio = indA.union(indB) #form the union of two index objects
+symm = indA.symmetric_difference(indB) #compute the symmetric difference of two Index objects
+
