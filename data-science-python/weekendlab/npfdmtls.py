dd5222c6ebfc1acb60127cb19860be5df7411f5f
benbasty <benbasty@yahoo.fr>
2026-08-06 14:49:05 +0800

    Add new section on reshaping, concatenating, and splitting arrays in npfdmtls.py

--- data-science-python/weekendlab/npfdmtls.py
+++ data-science-python/weekendlab/npfdmtls.py
@@ -44,3 +44,38 @@ readings[0, 0] = 18.2 # We set the first cell back to its original 18.2
 safe_window = readings[:, :2].copy() #we use .copy() to make a brand new, independent array with the same values.
 safe_window[0, 0] = -1.0
 assert readings[0, 0] == 18.2
+
+# Session 1.3 Reshape, concatenate, and split
+## Reshape: changing the dimennsion of your data without changing its numbers
+## Concatenate: putting arrays together
+## Split: cut one array into multiple pieces
+
+# create a list of numbers (a 1d array) from 0 to 23
+sequence = np.arange(24)
+# reshape the 1d array into a 3d array "cube" representing 2 days, 3 sites, 4 hours
+cube = sequence.reshape(2, 3, 4) # days, sites, hours
+# Uses an assert statement to double-check if the cube's day 1, site 2 and hour 3 is 23.
+assert cube[1, 2, 3] == 23
+
+# create a new day and glue it to the existing cube
+#create a 1d array of numbers from 0 to 11, add each item to 100 then reshape into a 3d arrays of 1 day, 3sites, 4 hours
+day_3 = (100 + np.arange(12)).reshape(1, 3, 4)
+# putting 2 days and day_3 together
+(2, 3, 4) + (1, 3, 4) = (3, 3, 4) # we now have 3 days.
+three_days = np.concatenate([cube, day_3], axis=0)
+
+# Cut the cube along the "sites" dimension into three separate pieces.
+site_a, site_b, site_c = np.split(three_days, [1, 2], axis=1)
+# [1, 2]:
+    ## Splits BEFORE index 1 (so piece 1 gets indices 0)
+    ## Splits BEFORE index 2 (so piece 2 gets indices 1)
+    ## Piece 3 gets everything from index 2 onward
+# axis=1: Split along the second dimension (the "sites" dimension).
+assert site_a.shape == site_b.shape == site_c.shape == (3, 1, 4)
+
+# Feature Creation: Use aggregation functions to build a 2D features table.
+    ## np.column_stack(): Takes two 1D arrays and stacks them as columns to make a 2D array.
+features = np.column_stack([readings.mean(1), readings.max(1)])
+assert features.shape == (3, 2)
+
+# PRACTICE TIME
