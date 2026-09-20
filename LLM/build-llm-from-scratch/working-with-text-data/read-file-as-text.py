e9583cb7290d041445c510db1502dace91cd263f
benbasty <benbasty@yahoo.fr>
2026-09-20 12:12:36 +0800

    Add GPTDatasetV1 and create_dataloader_v1 for batching inputs and targets

--- LLM/build-llm-from-scratch/working-with-text-data/read-file-as-text.py
+++ LLM/build-llm-from-scratch/working-with-text-data/read-file-as-text.py
@@ -158,6 +158,68 @@ for i in range(1, context_size+1):
 # and there's two tensors: an imput tensor containing the text that LLM sees and a target tensor that include the target for the llm to predict
 
 # pytorch will be used for efficient data loader implementation
+# the classes Dataset and DataLoader will be used
 
+# A dataset for batched inputs annd targets
+import torch  # pyright: ignore[reportMissingImports]
+from torch.utils.data import Dataset, DataLoader  # pyright: ignore[reportMissingImports]
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+# A data loader to generate batches with input targets pairs
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+# testing dataloader with a batch of size 1 for an LLM with a context of size of 4
+# to develop an intution of how GPTDatasetV1 and create_dataloader_v1 work together.
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+second_batch = next(data_iter)
+print(second_batch)
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4,
+    shuffle=False
+)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Inputs:\n", inputs)
+print("\nTargets:\n", targets)
 
 
